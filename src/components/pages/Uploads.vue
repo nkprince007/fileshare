ec693f40450177ddf842b6fdc0a7e03e692cffdf
Naveen Kumar Sangi <naveenkumarsangi@protonmail.com>
2018-04-12 11:31:24 +0530

    Send toast after successful upload

--- src/components/pages/Uploads.vue
+++ src/components/pages/Uploads.vue
@@ -6,10 +6,17 @@
           ref="dropzone"
           id="dropzone"
           :options="dropzoneOptions"
-          @vdropzone-complete="this.success"
+          @vdropzone-success="this.success"
         ></vue-dropzone>
       </v-flex>
     </v-layout>
+    <v-snackbar
+      :timeout="timeout"
+      top
+      v-model="snackbar"
+    >{{notification}}
+    <v-btn flat color="pink" @click.native="snackbar = false">Close</v-btn>
+    </v-snackbar>
   </v-container>
 </template>
 
@@ -23,17 +30,22 @@ export default {
   },
   data() {
     return {
+      notification: '',
+      snackbar: false,
+      timeout: 6000,
       dropzoneOptions: {
         url: "/upload",
         thumbnailWidth: 150,
-        maxFilesize: 0.5,
+        maxFilesize: 500,
         addRemoveLinks: true
       }
     }
   },
   methods: {
-    success () {
+    success (file) {
       this.$store.dispatch('getUserFiles')
+      this.notification = `${file.name} uploaded successfully`
+      this.snackbar = true
     }
   }
 };
